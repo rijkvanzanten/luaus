0bcfcaf5cf02240d3e170b2205a2a4dddaf85221
Rijk van Zanten <rijkvanzanten@me.com>
2017-04-13 13:19:31 +0200

    Move sockets connection to separate file

---
+++ nodemcu/socketmodule.lua
@@ -0,0 +1,35 @@
+local config = require 'config'
+
+local module = {}
+
+module.initSocket = function ()
+  -- Create socket connection
+  print('[WebSocket] Connecting to ws at ws://' .. config.ip .. ':' .. config.port .. '...')
+  local ws = websocket.createClient()
+
+  ws:on('connection', function(ws)
+    print('[WebSocket] Connected!')
+
+    ok, json = pcall(cjson.encode, {type = 'nodemcu', code = 0})
+    if ok then
+      ws:send(json)
+    else
+      print('[WebSocket] Failed to encode JSON!')
+    end
+  end)
+
+  ws:on('receive', function(_, msg)
+    print('[WebSocket] Got message: ', msg)
+  end)
+
+  ws:on('close', function(_, status)
+    print('[WebSocket] Connection closed.', status)
+    ws = nil
+  end)
+
+  ws:connect('ws://' .. config.ip .. ':' .. config.port)
+
+  return ws
+end
+
+return module
