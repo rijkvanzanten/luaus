c453a91d76b077009c3702fca7425c9cf8087b8d
Rijk van Zanten <rijkvanzanten@me.com>
2017-04-13 12:40:45 +0200

    replace socket.io with ws

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
+const http = require('http');
 const path = require('path');
 const express = require('express');
-const Socket = require('socket.io');
 const bodyParser = require('body-parser');
 const compression = require('compression');
+const WebSocket = require('ws');
 
 // Scores. Save in memory for know. Could be upgraded to a DB in the long run
 let player1 = 0;
@@ -14,14 +15,21 @@ const app = express()
   .use(bodyParser.urlencoded({extended: false}))
   .set('view engine', 'ejs')
   .set('views', path.join(__dirname, 'views'))
-  .get('/', getScoreBoard)
-  .listen(process.env.PORT || 3000, onListen);
+  .get('/', getScoreBoard);
 
-const io = new Socket(app);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
 
-io.on('connection', socket => {
-  console.log(socket.id);
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    message = JSON.parse(message);
+    console.log(message);
+  });
+
+  ws.send('something');
 });
+
+server.listen(process.env.PORT || 3000, onListen);
 
 function getScoreBoard(req, res) {
   if (req.query.id) {
