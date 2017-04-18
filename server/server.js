c353156a2eabdd6851a0e565367b9753760f154d
Rijk van Zanten <rijkvanzanten@me.com>
2017-04-18 12:25:59 +0200

    Strip everything from server and add DB

--- server/server.js
+++ server/server.js
@@ -1,88 +1,63 @@
 const http = require('http');
 const path = require('path');
 const express = require('express');
-const bodyParser = require('body-parser');
-const compression = require('compression');
+const low = require('lowdb');
 const WebSocket = require('ws');
 
-// Scores. Save in memory for know. Could be upgraded to a DB in the long run
-let scores = {};
+const port = process.env.PORT || 3000;
+
+const db = low('db.json');
+
+db.defaults({
+  players: []
+});
 
 const app = express()
-  .use(compression())
-  .use(bodyParser.urlencoded({extended: false}))
   .set('view engine', 'ejs')
-  .set('views', path.join(__dirname, 'views'))
-  .get('/', getScoreBoard)
-  .get('/reset', resetScore);
+  .set('views', path.join(__dirname))
+  .get('/', getApp);
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 
-wss.broadcast = data => {
-  wss.clients.forEach(client => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(data);
-    }
-  });
-};
+wss.broadcast = broadcast;
 
-wss.on('connection', ws => {
-  ws.on('message', message => {
-    message = JSON.parse(message);
-    console.log(message);
-    if (message.device === 'nodemcu') {
-      socketMcu(ws, message);
-    }
-  });
-});
+wss.on('connection', onSocketConnection);
 
-server.listen(process.env.PORT || 3000, onListen);
+server.listen(port, onListen);
 
-function socketMcu(ws, message) {
-  switch (message.code) {
-    // New button connection
-    case 0:
-      scores[message.id] = 0;
-      if (Object.keys(scores).length % 2) {
-        wss.broadcast(JSON.stringify({type: 'color', id: message.id, color: 'red'}));
-      } else {
-        wss.broadcast(JSON.stringify({type: 'color', id: message.id, color: 'blue'}));
-      }
-      break;
-    case 1:
-      scores[message.id]++;
-      wss.broadcast(JSON.stringify({type: 'score-update', id: message.id, score: scores[message.id]}));
-      break;
-  }
-}
-
-function resetScore(req, res) {
-  scores = {};
-  res.redirect('/');
-}
-
-function getScoreBoard(req, res) {
-  // if (req.query.id) {
-  //   if (req.query.id === '2936046') {
-  //     player1++;
-  //   } else {
-  //     player2++;
-  //   }
-  //
-  //   if (player1 >= 8) {
-  //     winner = 'p1-win';
-  //   } else if (player2 >= 8) {
-  //     winner = 'p2-win';
-  //   } else {
-  //     winner = false;
-  //   }
-  //
-  //   io.emit('score', {player1, player2, winner});
-  // }
-  res.render('scoreboard', {scores});
+function getApp(req, res) {
+  res.render('index');
 }
 
 function onListen() {
-  console.log('Server started at port ' + process.env.PORT || 3000);
+  console.log('Server started at port ' + port);
+}
+
+function broadcast(data) {
+  wss.clients.forEach(sendData);
+
+  function sendData(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  }
+}
+
+function onSocketConnection(socket) {
+  socket.on('message', onSocketMessage);
+
+  function onSocketMessage(message) {
+    try {
+      message = JSON.parse(message);
+    } catch (err) {
+      console.log(`Cannot parse message: ${message}`, err);
+    }
+
+    if (message.device === 'nodemcu') {
+
+    } else if (message.device === 'client') {
+
+    }
+  }
 }
