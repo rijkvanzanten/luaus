78788fa3201a5f20ced4bc3502ed9a4fca8add81
Rijk van Zanten <rijkvanzanten@me.com>
2017-04-20 15:36:11 +0200

    Fix server code w/ prettier

--- app/server.js
+++ app/server.js
@@ -23,7 +23,7 @@ const colors = [
 ];
 
 const app = express()
-  .use(bodyParser.urlencoded({extended: false}))
+  .use(bodyParser.urlencoded({ extended: false }))
   .use(express.static(path.join(__dirname, 'public')))
   .set('view engine', 'ejs')
   .set('views', path.join(__dirname, 'views'))
@@ -32,7 +32,7 @@ const app = express()
   .post('/controller', postController);
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({server});
+const wss = new WebSocket.Server({ server });
 
 wss.broadcast = broadcast;
 
@@ -41,11 +41,11 @@ wss.on('connection', onSocketConnection);
 server.listen(port, onListen);
 
 function getApp(req, res) {
-  res.render('index', {maxScore: game.maxScore, started: game.started});
+  res.render('index', { maxScore: game.maxScore, started: game.started });
 }
 
 function getController(req, res) {
-  res.render('controller', {player: false});
+  res.render('controller', { player: false });
 }
 
 function postController(req, res) {
@@ -55,12 +55,21 @@ function postController(req, res) {
   } else {
     color = colors[Object.keys(game.players).length % colors.length];
     game.players[req.body.name] = {
-      color, score: 0
+      color,
+      score: 0
     };
   }
 
-  wss.broadcast(JSON.stringify({action: 'NEW_PLAYER', player: game.players[req.body.name]}));
-  res.render('controller', {player: game.players[req.body.name], name: req.body.name});
+  wss.broadcast(
+    JSON.stringify({
+      action: 'NEW_PLAYER',
+      player: game.players[req.body.name]
+    })
+  );
+  res.render('controller', {
+    player: game.players[req.body.name],
+    name: req.body.name
+  });
 }
 
 function onListen() {
@@ -88,8 +97,10 @@ function onSocketConnection(socket) {
     }
 
     switch (message.device) {
-      case 'nodemcu': return nodemcuMessage(socket, message);
-      case 'scoreboard': return scoreboardMessage(socket, message);
+      case 'nodemcu':
+        return nodemcuMessage(socket, message);
+      case 'scoreboard':
+        return scoreboardMessage(socket, message);
       default:
         console.log('Device not recognized: ', message.device);
     }
@@ -107,17 +118,26 @@ function nodemcuMessage(socket, message) {
       } else {
         color = colors[Object.keys(game.players).length % colors.length];
         game.players[message.id] = {
-          color, score: 0
+          color,
+          score: 0
         };
       }
 
-      wss.broadcast(JSON.stringify({action: 'NEW_PLAYER', player: game.players[message.id]}));
+      wss.broadcast(
+        JSON.stringify({
+          action: 'NEW_PLAYER',
+          player: game.players[message.id]
+        })
+      );
 
-      return socket.send(JSON.stringify({
-        action: 'CHANGE_COLOR',
-        color
-      }));
-    default: return false;
+      return socket.send(
+        JSON.stringify({
+          action: 'CHANGE_COLOR',
+          color
+        })
+      );
+    default:
+      return false;
   }
 }
 
@@ -127,14 +147,19 @@ function scoreboardMessage(socket, message) {
   switch (message.action) {
     case 'SET_MAX_SCORE':
       game.maxScore = message.maxScore;
-      wss.broadcast(JSON.stringify({action: 'SET_MAX_SCORE', maxScore: game.maxScore}));
+      wss.broadcast(
+        JSON.stringify({ action: 'SET_MAX_SCORE', maxScore: game.maxScore })
+      );
       break;
     case 'MOVE_MOUSE':
       wss.broadcast(JSON.stringify(message));
-    default: return false;
+    default:
+      return false;
     case 'START_GAME':
       game.started = true;
-      wss.broadcast(JSON.stringify({action: 'START_GAME', maxScore: game.maxScore}));
+      wss.broadcast(
+        JSON.stringify({ action: 'START_GAME', maxScore: game.maxScore })
+      );
       break;
   }
 }
