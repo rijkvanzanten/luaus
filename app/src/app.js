433af1991d0088e51cc2e95575381027ba93d33c
BerendPronk <berendpronk@quicknet.nl>
2017-04-27 19:26:53 +0200

    Bundle controller script with app.js

--- app/src/app.js
+++ app/src/app.js
@@ -6,145 +6,190 @@ const shortid = require('shortid');
   const ws = new WebSocket('ws://' + location.hostname + ':' + location.port);
   const id = shortid.generate();
 
-  ws.addEventListener('open', onOpenSocket);
-  ws.addEventListener('message', onSocketMessage);
-  document.body.addEventListener('mousemove', throttle(10, onMouseMove));
+  if (document.body.classList.contains('controller')) {
+    const btn = document.querySelector('#bigredbutton');
+    const btnID = document.querySelector('h1').innerText;
 
-  document.querySelector('form').addEventListener('submit', onFormSubmit);
+    ws.addEventListener('open', onOpenSocket);
+    ws.addEventListener('message', onSocketMessage);
 
-  document
-    .querySelectorAll('[type=button]')
-    .forEach(button => button.addEventListener('click', onButtonClick));
+    btn.addEventListener('click', updateScore);
 
-  function onFormSubmit(e) {
-    e.preventDefault();
-    ws.send(
-      JSON.stringify({
-        device: 'scoreboard',
-        action: 'START_GAME',
-        maxScore: Number(document.querySelector('input').value)
-      })
-    );
-    return false;
-  }
+    function onOpenSocket() {
+      console.log('Socket connected');
+      btn.disabled = true;
+    }
 
-  function onOpenSocket() {
-    console.log('Socket connected');
-  }
+    function onSocketMessage(event) {
+      const data = JSON.parse(event.data);
 
-  function onSocketMessage(event) {
-    const data = JSON.parse(event.data);
-    switch (data.action) {
-      case 'SET_MAX_SCORE':
-        document.querySelector('input').value = data.maxScore;
-        break;
-      case 'MOVE_MOUSE':
-        if (data.id !== id) {
-          const element = document.querySelector(
-            `div[data-clientid="${data.id}"]`
-          );
-          if (!element) {
-            document.body.innerHTML += `<div data-clientid="${data.id}"></div>`;
+      switch (data.action) {
+        case 'START_GAME':
+          btn.disabled = false;
+          break;
+        case 'END_GAME':
+          btn.disabled = true;
+          // btn.removeEventListener('click', updateScore);
+
+          if (btnID !== data.winner.id) {
+            document.querySelector('.controller').classList.add('lost');
           }
+          break;
+        default:
+          return false;
+      }
+    }
 
-          document.querySelector(
-            `div[data-clientid="${data.id}"]`
-          ).style.transform = `translate(${data.clientX}px, ${data.clientY}px)`;
-          document.querySelector(
-            `div[data-clientid="${data.id}"]`
-          ).style.animation =
-            'none'; // 💩
-          setTimeout(() => {
+    function updateScore() {
+      ws.send(
+        JSON.stringify({
+          device: 'phone',
+          action: 'UPDATE_SCORE',
+          id: btnID
+        })
+      )
+    }
+  } else {
+    ws.addEventListener('open', onOpenSocket);
+    ws.addEventListener('message', onSocketMessage);
+    document.body.addEventListener('mousemove', throttle(10, onMouseMove));
+
+    document.querySelector('form').addEventListener('submit', onFormSubmit);
+
+    document
+      .querySelectorAll('[type=button]')
+      .forEach(button => button.addEventListener('click', onButtonClick));
+
+    function onFormSubmit(e) {
+      e.preventDefault();
+      ws.send(
+        JSON.stringify({
+          device: 'scoreboard',
+          action: 'START_GAME',
+          maxScore: Number(document.querySelector('input').value)
+        })
+      );
+      return false;
+    }
+
+    function onOpenSocket() {
+      console.log('Socket connected');
+    }
+
+    function onSocketMessage(event) {
+      const data = JSON.parse(event.data);
+      switch (data.action) {
+        case 'SET_MAX_SCORE':
+          document.querySelector('input').value = data.maxScore;
+          break;
+        case 'MOVE_MOUSE':
+          if (data.id !== id) {
+            const element = document.querySelector(
+              `div[data-clientid="${data.id}"]`
+            );
+            if (!element) {
+              document.body.innerHTML += `<div data-clientid="${data.id}"></div>`;
+            }
+
+            document.querySelector(
+              `div[data-clientid="${data.id}"]`
+            ).style.transform = `translate(${data.clientX}px, ${data.clientY}px)`;
             document.querySelector(
               `div[data-clientid="${data.id}"]`
             ).style.animation =
-              'fadeout .5s linear forwards';
-          }, 10);
-        }
-        break;
-      case 'NEW_PLAYER':
-        const playerList = document.querySelector('#players ul');
-        const newPlayer = document.createElement('li');
-        const playerType = document.createElement('img');
-        const playerName = document.createElement('h2');
-        const playerScore = document.createElement('span');
-        const colors = data.player.color;
+              'none'; // 💩
+            setTimeout(() => {
+              document.querySelector(
+                `div[data-clientid="${data.id}"]`
+              ).style.animation =
+                'fadeout .5s linear forwards';
+            }, 10);
+          }
+          break;
+        case 'NEW_PLAYER':
+          const playerList = document.querySelector('#players ul');
+          const newPlayer = document.createElement('li');
+          const playerType = document.createElement('img');
+          const playerName = document.createElement('h2');
+          const playerScore = document.createElement('span');
+          const colors = data.player.color;
 
-        newPlayer.style.backgroundColor = `rgb(${colors[1]}, ${colors[0]}, ${colors[2]})`;
-        newPlayer.setAttribute('data-id', data.player.id);
-        newPlayer.setAttribute('data-type', data.player.type);
+          newPlayer.style.backgroundColor = `rgb(${colors[1]}, ${colors[0]}, ${colors[2]})`;
+          newPlayer.setAttribute('data-id', data.player.id);
+          newPlayer.setAttribute('data-type', data.player.type);
 
-        playerType.src = `/${data.player.type}.png`;
-        playerType.alt = `Player uses ${data.player.type}`;
+          playerType.src = `/${data.player.type}.png`;
+          playerType.alt = `Player uses ${data.player.type}`;
 
-        playerName.textContent = data.player.id;
-        playerScore.textContent = data.player.score;
+          playerName.textContent = data.player.id;
+          playerScore.textContent = data.player.score;
 
-        newPlayer.appendChild(playerType);
-        newPlayer.appendChild(playerName);
-        newPlayer.appendChild(playerScore);
-        playerList.appendChild(newPlayer);
-        break;
-      case 'START_GAME':
-        document.querySelector('body').classList.add('started');
-        break;
-      case 'UPDATE_SCORE':
-        const player = document.querySelector(`#players li[data-id="${data.id}"]`);
-        const score = player.querySelector('span');
+          newPlayer.appendChild(playerType);
+          newPlayer.appendChild(playerName);
+          newPlayer.appendChild(playerScore);
+          playerList.appendChild(newPlayer);
+          break;
+        case 'START_GAME':
+          document.body.classList.add('started');
+          break;
+        case 'UPDATE_SCORE':
+          const player = document.querySelector(`#players li[data-id="${data.id}"]`);
+          const score = player.querySelector('span');
 
-        score.textContent = Number(score.textContent) + 1;
-        break;
-      case 'END_GAME':
-        const players = document.querySelectorAll('#players li');
-        const winner = data.winner.id;
+          score.textContent = Number(score.textContent) + 1;
+          break;
+        case 'END_GAME':
+          const players = document.querySelectorAll('#players li');
+          const winner = data.winner.id;
 
-        players
-          .forEach(player => {
-            if (player.getAttribute('data-id') === String(winner)) {
-              player.classList.add('won');
-            } else {
-              player.classList.add('lost');
-            }
-          });
+          players
+            .forEach(player => {
+              if (player.getAttribute('data-id') === String(winner)) {
+                player.classList.add('won');
+              } else {
+                player.classList.add('lost');
+              }
+            });
 
-        break;
-      default:
-        return false;
-    }
-  }
-
-  function onMouseMove(e) {
-    ws.send(
-      JSON.stringify({
-        device: 'scoreboard',
-        id,
-        action: 'MOVE_MOUSE',
-        clientX: e.clientX,
-        clientY: e.clientY,
-        innerWidth: window.innerWidth,
-        innerHeight: window.innerHeight
-      })
-    );
-  }
-
-  function onButtonClick(e) {
-    let value = Number(document.querySelector('input').value);
-
-    if (e.target.id === 'min') {
-      value--;
-    } else {
-      value++;
+          break;
+        default:
+          return false;
+      }
     }
 
-    document.querySelector('input').value = value;
+    function onMouseMove(e) {
+      ws.send(
+        JSON.stringify({
+          device: 'scoreboard',
+          id,
+          action: 'MOVE_MOUSE',
+          clientX: e.clientX,
+          clientY: e.clientY,
+          innerWidth: window.innerWidth,
+          innerHeight: window.innerHeight
+        })
+      );
+    }
 
-    ws.send(
-      JSON.stringify({
-        device: 'scoreboard',
-        id,
-        action: 'SET_MAX_SCORE',
-        maxScore: value
-      })
-    );
+    function onButtonClick(e) {
+      let value = Number(document.querySelector('input').value);
+
+      if (e.target.id === 'min') {
+        value--;
+      } else {
+        value++;
+      }
+
+      document.querySelector('input').value = value;
+
+      ws.send(
+        JSON.stringify({
+          device: 'scoreboard',
+          id,
+          action: 'SET_MAX_SCORE',
+          maxScore: value
+        })
+      );
+    }
   }
 })();
