2a9867a26fbe2ade485a19c9e22c2f2b5941979b
Rijk van Zanten <rijkvanzanten@me.com>
2017-05-02 15:17:59 +0200

    Add className to new-player view

--- app/views/new-player.js
+++ app/views/new-player.js
@@ -1,11 +1,13 @@
 const h = require('virtual-dom/h');
 
 module.exports = function (data) {
-  return h('form', {method: 'post', action: '/new-player/' + data}, [
-    h('label', [
-      'Enter your name:',
-      h('input', {type: 'text', name: 'name'})
-    ]),
-    h('button', {type: 'submit'}, 'Join Game!')
+  return h('div', {className: 'new-player'}, [
+    h('form', {method: 'post', action: '/new-player/' + data}, [
+      h('label', [
+        'Enter your name:',
+        h('input', {type: 'text', name: 'name'})
+      ]),
+      h('button', {type: 'submit'}, 'Join Game!')
+    ])
   ]);
 };
