0b731ae30cb60bc76429890e518a04d8f6f24811
Rijk van Zanten <rijkvanzanten@me.com>
2017-05-03 16:09:51 +0200

    Use object instead of string

--- app/views/new-player.js
+++ app/views/new-player.js
@@ -2,7 +2,7 @@ const h = require('virtual-dom/h');
 
 module.exports = function (data) {
   return h('div', {className: 'new-player'}, [
-    h('form', {method: 'post', action: '/new-player/' + data}, [
+    h('form', {method: 'post', action: '/new-player/' + data.gameID}, [
       h('label', [
         'Enter your name:',
         h('input', {type: 'text', name: 'name'})
