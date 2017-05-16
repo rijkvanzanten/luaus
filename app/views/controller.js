c38681ea014841674c121cebb59942fca7fec46c
Rijk van Zanten <rijkvanzanten@me.com>
2017-05-16 19:07:19 +0200

    Add won class to controller on win

--- app/views/controller.js
+++ app/views/controller.js
@@ -3,7 +3,7 @@ const audio = require('./audio');
 
 module.exports = function (data) {
   const {name, gameID, playerID, game} = data;
-  return h('div', {className: 'controller'}, [
+  return h('div', {className: `controller ${game.ended && game.winner === playerID ? 'won' : 'lost'}`}, [
     game.ended && game.winner === playerID ? audio(true, '/game-win.mp3') : audio(),
     h('h1', name),
     h('form', {method: 'post', action: `/${gameID}/${playerID}`}, [
