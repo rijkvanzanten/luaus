597cb6e430379dc0e3f11c1581b48f312035cdd6
Rijk van Zanten <rijkvanzanten@me.com>
2017-05-17 10:41:10 +0200

    Fix background won animation

--- app/views/controller.js
+++ app/views/controller.js
@@ -6,12 +6,15 @@ module.exports = function (data) {
 
   return h('div', {
     className: `controller ${game.ended ? game.winner === playerID ? 'won' : 'lost' : ''}`,
-    style: {
-      backgroundColor: `rgb(${game.players[playerID].color[1]}, ${game.players[playerID].color[0]}, ${game.players[playerID].color[2]})`
-    }
   }, [
     game.ended && game.winner === playerID ? audio(true, '/game-win.mp3') : audio(),
     h('h1', name),
+    h('div', {
+      className: 'back',
+      style: {
+        backgroundColor: `rgb(${game.players[playerID].color[1]}, ${game.players[playerID].color[0]}, ${game.players[playerID].color[2]})`
+      }
+    }),
     h('form', {method: 'post', action: `/${gameID}/${playerID}`}, [
       h('button', {id: 'bigredbutton'}),
       h('input', {type: 'hidden', name: 'gameID', value: gameID}),
