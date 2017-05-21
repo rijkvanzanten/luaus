3947d2cc69902a2662d13d78f661abaaf4d6ea92
Rijk van Zanten <rijkvanzanten@me.com>
2017-05-21 15:58:49 +0200

    [WIP] try to fix tweetstream

--- app/server.js
+++ app/server.js
@@ -41,7 +41,7 @@ if (
     });
   };
 
-  const stream = TweetStream({
+  const stream = new TweetStream({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     token: process.env.TWITTER_ACCESS_TOKEN_KEY,
