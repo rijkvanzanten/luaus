b57e995d4d7d2042f06f3d5d075652d599781ff9
Rijk van Zanten <rijkvanzanten@me.com>
2017-05-21 15:42:11 +0200

    Add hashtag to messages

--- app/tweets.js
+++ app/tweets.js
@@ -1,21 +1,21 @@
 module.exports = {
   startGame(gameName) {
     const messages = [
-      `Game ${gameName} is about to begin! #luaus`
+      `Game ${gameName} is about to begin! #luaus_live`
     ];
 
     return messages[Math.floor(Math.random() * messages.length)];
   },
   endGame(winner, gameName) {
     const messages = [
-      `${winner} is the grand winner of ${gameName}! #luaus`
+      `${winner} is the grand winner of ${gameName}! #luaus_live`
     ];
 
     return messages[Math.floor(Math.random() * messages.length)];
   },
   newGame(gameName) {
     const messages = [
-      `Come and join ${gameName}`
+      `Come and join ${gameName} #luaus_live`
     ];
 
     return messages[Math.floor(Math.random() * messages.length)];
