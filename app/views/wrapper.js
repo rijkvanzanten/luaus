7a7eff56963f751103ed08ecff0538a00816bae6
Rijk van Zanten <rijkvanzanten@me.com>
2017-05-23 11:02:08 +0200

    Add favicons

--- app/views/wrapper.js
+++ app/views/wrapper.js
@@ -8,6 +8,14 @@ module.exports = function (body, data, refresh = false) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Luaus!</title>
     <link rel="stylesheet" href="/style.css" />
+    <link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
+    <link rel="manifest" href="/icons/manifest.json">
+    <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#efbcbb">
+    <link rel="shortcut icon" href="/icons/favicon.ico">
+    <meta name="msapplication-config" content="/icons/browserconfig.xml">
+    <meta name="theme-color" content="#efbcbb">
     <script src="/socket.io/socket.io.js"></script>
     <script src="/app.js" defer></script>
     ${refreshEl}
